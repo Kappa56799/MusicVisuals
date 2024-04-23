5779caeb9615cb116ef944fee367306d952f4a4f
Kappa56799 <Kacper200657@hotmail.com>
2024-04-23 18:28:45 +0100

    New Cube animation

--- java/src/MusicVis/rotatingcube.java
+++ java/src/MusicVis/rotatingcube.java
@@ -2,8 +2,9 @@ package MusicVis;
 
 public class rotatingcube {
     int lastColorChange = 0;
-    int colorChangeInterval = 1000; // Change color every 1 second
+    int colorChangeInterval = 1500; // Change color every 1 second
     int[] cubeColors; // Array to store the hue of each cube
+    int[] nextColors; // Array to store the hue of each cube
     MyVisual mv;
     float a;                 // Angle of rotation
     float offset;            // Angle offset between boxes
@@ -13,16 +14,21 @@ public class rotatingcube {
         this.mv = mv;
         offset = (float)(mv.PI / 24.0);
         cubeColors = new int[num];
+        nextColors = new int[num];
         initializeCubeColors(); // Initialize cube colors
     }
 
     void initializeCubeColors() {
         for (int i = 0; i < num; i++) {
             cubeColors[i] = (int) mv.random(360); // Initialize each cube with a random hue
+            nextColors[i] = (int) mv.random(360); // Initialize each cube with a random hue
         }
+
     }
 
     void render() {
+
+        float amt = (float)(mv.millis() - lastColorChange) / colorChangeInterval;
         mv.pushMatrix();
         mv.colorMode(mv.HSB, 360, 100, 100);
         mv.translate(mv.width/2, mv.height/2 - 150); 
@@ -35,7 +41,7 @@ public class rotatingcube {
         }
 
         for(int i = 0; i < num; i++) {
-            mv.fill(cubeColors[i], 100, 100); // Set fill color using HSB color mode
+            mv.fill(mv.lerpColor(cubeColors[i], nextColors[i], amt), 100, 100); // Set fill color using HSB color mode
             mv.pushMatrix();
             mv.rotateY(a + offset*i);
             mv.rotateX(a/2 + offset*i);
