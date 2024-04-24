b37fc10324b8b6f8e92216cd4e9934a1279b3f00
Pavels <pavelzs7@mail.com>
2024-04-24 16:55:35 +0100

    waveform and matrix update

---
+++ java/src/MusicVis/waveForm.java
@@ -0,0 +1,23 @@
+package MusicVis;
+
+public class waveForm {
+
+    MyVisual mv;
+    int totalPts = 10000;
+    float steps = totalPts + 1;
+  
+    void setup() {
+    mv.stroke(255);
+    mv.frameRate(1);
+    } 
+
+    void draw() {
+    float rand = 0;
+    for  (int i = 1; i < steps; i++) {
+        mv.point( (mv.width/steps) * i, (mv.height/2) + mv.random(-rand, rand) );
+        rand += mv.random(-5, 5);
+    }
+}
+
+
+}
