aa314e695d642ec9e22dc04ee220d54f638c40f3
Kappa56799 <Kacper200657@hotmail.com>
2024-04-24 19:11:59 +0100

    fixed matrix speed

--- java/src/MusicVis/matrix.java
+++ java/src/MusicVis/matrix.java
@@ -9,31 +9,35 @@ public class matrix {
     MyVisual mv;
     PFont theFont;
     ArrayList<ArrayList<CharInfo>> streams;
+    long lastChangeTime = 0; // Variable to store the last time color or size was changed
+    int interval = 500; // Interval in milliseconds for color and size change
+    int lastRenderedFrame = -1; // Last frame when rendering occurred
 
     public matrix(MyVisual mv) {
         this.mv = mv;
     }
 
     public void render() {
-        theFont = mv.createFont("Arial Unicode MS", 20);
-        mv.textFont(theFont);
-        mv.textAlign(PApplet.CENTER, PApplet.TOP);
+        if (mv.millis() > lastChangeTime + interval) { // Render every second
+            theFont = mv.createFont("Arial Unicode MS", 20);
+            mv.textFont(theFont);
+            mv.textAlign(PApplet.CENTER, PApplet.TOP);
 
-        mv.println(Integer.parseInt("30A1", 16));
-        mv.println(Integer.parseInt("3147", 16));
+            streams = new ArrayList<>();
 
-        streams = new ArrayList<>();
+            for (int x = 10; x < mv.width; x += 35) {
+                ArrayList<CharInfo> chars = new ArrayList<>();
+                int numChar = mv.round(mv.random(2, 40));
+                float speed = 0.1f;
 
-        for (int x = 10; x < mv.width; x += 35) {
-            ArrayList<CharInfo> chars = new ArrayList<>();
-            int numChar = mv.round(mv.random(2, 40));
-            int speed = mv.round(mv.random(50, 150));
-
-            for (int y = 0; y < numChar * 20; y += 20) {
-                char c = updateStreams();
-                chars.add(new CharInfo(x, y, speed, c));
+                for (int y = 0; y < numChar * 20; y += 20) {
+                    char c = updateStreams();
+                    chars.add(new CharInfo(x, y, speed, c));
+                }
+                streams.add(chars);
             }
-            streams.add(chars);
+
+            lastChangeTime = mv.millis(); // Update last change time= mv.frameCount; // Update last rendered frame
         }
 
         draw();
@@ -42,7 +46,7 @@ public class matrix {
 
     public void draw() {
         mv.background(0, 50, 0);
-    
+
         for (ArrayList<CharInfo> chars : streams) {
             int numChars = chars.size();
             for (int i = 0; i < numChars; i++) {
@@ -53,24 +57,14 @@ public class matrix {
                 }
                 mv.text(chars.get(i).c, chars.get(i).x, chars.get(i).y);
                 chars.get(i).y += chars.get(i).speed; // Adjust character movement speed
-    
-                // Check if it's time to update character
-                if (mv.millis() - chars.get(i).lastUpdateTime >= chars.get(i).changeSpeed) {
-                    if (i == numChars - 1) {
-                        chars.get(i).c = updateStreams();
-                    } else {
-                        chars.get(i).c = chars.get(i + 1).c;
-                    }
-                    chars.get(i).lastUpdateTime = mv.millis(); // Update last update time
-                }
-    
+
                 if (chars.get(i).y > mv.height) { // Check if character has reached the bottom
                     chars.get(i).y = 0; // Reset character position to the top
                 }
             }
         }
     }
-    
+
 
     public char updateStreams() {
         int charType = mv.round(mv.random(1));
@@ -86,10 +80,11 @@ public class matrix {
     class CharInfo {
         public int changeSpeed;
         public int lastUpdateTime;
-        int x, y, speed;
+        int x, y;
+        float speed;
         char c;
 
-        CharInfo(int x, int y, int speed, char c) {
+        CharInfo(int x, int y, float speed, char c) {
             this.x = x;
             this.y = y;
             this.speed = speed;
